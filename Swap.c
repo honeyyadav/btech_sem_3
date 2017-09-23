c7d4d3c47362ff052bd0a69d25c5bb6ceed35a78
honeyyadav <honeyyadav@icloud.com>
2017-09-23 21:27:11 +0530

    SWAP FILE UPLOADED LANGUAGE-C
    As this is a swapping program so the program takes two values as input. The value of x an the value of y. Where x and y are two variables. And then it swaps the values of x and y and gives the result. This program uses three variables x, y and temp. 
    Logic:-
    temp=x; // assigns value of x in temp
    x=y; // assigns value of y in x
    y=temp; // assign value of temp(which is the value of x) in y
     
    In the next version we will use only two variables x and y.

---
+++ Swap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int x, y, temp;
+	printf("Welcome sir, this program takes two values as input.\n");
+	printf("1. The value of x.\n");
+	printf("2. The value of y.\n");
+	printf("And then it swaps them and gives the result.\n\n");
+	system("pause");
+	system("cls");
+	printf("Well, enter the value of x.->");
+	scanf("%d",&x);
+	printf("Ok, enter the value of y now.->");
+	scanf("%d",&y);
+	//---------logic------------
+	temp=x;
+	x=y;
+	y=temp;
+	//-------logic end----------
+	printf("\n************RESULT************\n\n");
+	printf("Now the value of x becomes: %d\n",x);
+	printf("Now the value of y becomes: %d\n",y);
+	
+}
