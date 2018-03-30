51e7557d41a171d71b1f038b6be43ebefe088caa
honeyyadav <mr.honeyyadav@gmail.com>
2018-03-30 23:28:30 +0530

    Question number 1
    Question number 1 of practical file is programmed.

---
+++ CPP_Practical_4th_Sem/1.Raising_Power.cpp
@@ -0,0 +1,26 @@
+/****************************************************
+*  AUTHOR:                  HONEY YADAV             *
+*  PROGRAM:                 QUEUE                   *
+*  IN COORDINATION WITH:    DR. SATINDER BAL GUPTA  *
+*  INSTITUITION:            IGU MEERPUR             *
+****************************************************/
+#include <iostream>
+using namespace std;
+double power(double n,int p=2){
+	double ans=1;
+	for(int i=1;i<=p;i++){
+		ans*=n;
+	}
+	return ans;
+}
+int main(){
+	double num;
+	int p;
+	cout<<"Enter a number whose power should be calculated.->";
+	cin>>num;
+	cout<<num<<"^"<<"2"<<" is "<<power(num)<<endl;
+	cout<<"Enter power->";
+	cin>>p;
+	cout<<num<<"^"<<p<<" is "<<power(num,p)<<endl;
+	return 0;
+}	
