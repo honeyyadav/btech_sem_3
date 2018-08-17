611f5ba2551b5a419139ee8f560e773c88416287
honeyyadav <mr.honeyyadav@gmail.com>
2018-08-17 20:32:53 +0530

    Digital Differential Analyzer Algorithm Program

---
+++ 5th_Sem/DDA_Algorithm.cpp
@@ -0,0 +1,35 @@
+#include <graphics.h>
+#include <conio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    double x1,y1,x2,y2;
+    cout<<"Please enter first point(x1,y1)>";
+    cin>>x1>>y1;
+    cout<<"Please enter second point(x2,y2)>";
+    cin>>x2>>y2;
+    double dx=x2-x1;
+    double dy=y2-y1;
+    int steps;
+    if(dx>dy){
+        steps=(int)dx;
+    }
+    else{
+        steps=(int)dy;
+    }
+    double xinc=(double)dx/steps;
+    double yinc=(double)dy/steps;
+    cout<<"x increment="<<xinc<<" y increment="<<yinc<<endl;
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"");
+    for(int i=0;i<steps;i++){
+        putpixel(round(x1),round(y1),WHITE);
+        cout<<round(x1)<<" "<<round(y1)<<endl;
+        x1=x1+xinc;
+        y1=y1+yinc;
+    }
+    getch();
+    closegraph();
+    return 0;
+}
