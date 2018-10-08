fa708f0a2e64159c5c5c063baca37240873c6f8a
Honey Yadav <mr.honeyyadav@gmail.com>
2018-10-08 21:00:23 +0530

    Update Circle.cpp

--- 5th_Sem/Circle.cpp
+++ 5th_Sem/Circle.cpp
@@ -1,13 +1,33 @@
+/**********************************************
+*   AUTHOR:                  HONEY YADAV      *
+*   PROGRAM:                 CIRCLE           *
+*   IN COORDINATION WITH:    MUSKAN MAM       *
+*   INSTITUITION:            IGU MEERPUR      *
+**********************************************/
+
 #include<iostream>
 #include<graphics.h>
 using namespace std;
+void setBackground(int);
 int main()
 {
-	int gd, gm;
+	int gd=DETECT, gm, x, y, r;
 	detectgraph(&gd, &gm);
 	initgraph(&gd, &gm,"");
-	circle(100,100,100);
+	setBackground(WHITE);
+	setcolor(BLACK);
+	cout<<"Enter center coordinates of circle(x,y)->";
+	cin>>x>>y;
+	cout<<"Enter radius of circle(x,y)->";
+    cin>>r;
+	circle(x,y,r);
 	getch();
 
 	return 0;
+}
+void setBackground(int colorcode){
+    setcolor(colorcode);
+    for(int i=0;i<480;i++){
+        line(0,i,640,i);
+    }
 }
